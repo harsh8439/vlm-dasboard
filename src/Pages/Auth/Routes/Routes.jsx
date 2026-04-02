cb8eb52a457489c62991d8b2c6ebb5323b88b305
harsh8439 <harshsharma@pearlorganisation.com>
2026-04-02 11:29:57 +0530

    first code

---
+++ src/Pages/Auth/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../Login";
+
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+    
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
